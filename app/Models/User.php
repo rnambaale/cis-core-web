ac0ffcff39a41ed722b21934623a36d215f9b137
mtvbrianking <mtvbrianking@gmail.com>
2019-10-17 16:46:40 +0300

    Use UUID for user models.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,42 @@
 
 namespace App\Models;
 
+use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -35,4 +65,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model): void {
+            if (! $model->getKey()) {
+                $model->{$model->getKeyName()} = Uuid::uuid4()->toString();
+            }
+        });
+    }
 }
