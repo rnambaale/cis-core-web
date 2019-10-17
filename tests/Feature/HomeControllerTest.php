9897d9cc6aeb207587c2f89ff664e51fb51f3b12
mtvbrianking <mtvbrianking@gmail.com>
2019-10-17 16:46:58 +0300

    Test basic auth.

---
+++ tests/Feature/HomeControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+/**
+ * @see \App\Http\Controllers\HomeController
+ */
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_cant_visit_home_unauthenticated()
+    {
+        $this->get('/')->assertStatus(302)->assertRedirect(route('login'));
+    }
+
+    public function test_can_visit_home()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/');
+
+        $response->assertStatus(200);
+    }
+}
