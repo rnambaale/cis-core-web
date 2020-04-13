8e5ee628a1eaf1a43cdc13bbd56e6258779fdec5
Raymond Nambaale <nambaaler@gmail.com>
2020-04-13 18:38:13 +0300

    Dect current menu section

--- resources/views/pharmacy/nav.blade.php
+++ resources/views/pharmacy/nav.blade.php
@@ -1,11 +1,11 @@
 <ul class="sections-nav text-center d-flex">
-    <li class="{{ (Route::is('pharmacy.inventories.index', $storeId))  ? 'active' : '' }}">
+    <li class="{{ ($section == 'inventory')  ? 'active' : '' }}">
         <a href="{{ route('pharmacy.inventories.index', $storeId) }}" title="Inventory">Inventory</a>
     </li>
-    <li class="{{ (Route::is('pharmacy.sales.create', $storeId))  ? 'active' : '' }}">
+    <li class="{{ ($section == 'sales')  ? 'active' : '' }}">
         <a href="{{ route('pharmacy.sales.create', $storeId) }}" title="Sales">Sales</a>
     </li>
-    <li class="{{ (Route::is('pharmacy.purchases.create', $storeId))  ? 'active' : '' }}">
+    <li class="{{ ($section == 'purchases')  ? 'active' : '' }}">
         <a href="{{ route('pharmacy.purchases.create', $storeId) }}" title="Purchases">Purchases</a>
     </li>
 </ul>
